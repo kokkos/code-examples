d1d46ccdd73c8a940285e2e4363164612dc9daad
Christian Trott <crtrott@sandia.gov>
2021-04-20 18:11:33 -0600

    Adding Native CUDA/OMP/OMPT variants to atomic example in tpds paper

--- papers/kokkos-core-tpds-2021/atomic/atomic.cpp
+++ papers/kokkos-core-tpds-2021/atomic/atomic.cpp
@@ -45,16 +45,70 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Random.hpp>
 
+#if !defined(USE_OMP) && !defined(USE_CUDA) && !defined(USE_KOKKOS) && !(USE_OMPT)
+#define USE_KOKKOS
+#endif
+
 using particle_pos_t = Kokkos::View<double*[3]>;
+
+#if defined(USE_CUDA)
+template<class LAMBDA>
+__global__ void cuda_kernel(size_t N, const LAMBDA f) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if(i<N) f(i);
+}
+#endif
 
 template<class particle_val_t, class grid_t>
 void discretize(double dx, particle_pos_t pos, particle_val_t value, grid_t grid) {
+#if defined(USE_KOKKOS)
   Kokkos::parallel_for("Deposit", pos.extent(0), KOKKOS_LAMBDA(int i) {
     int x = pos(i,0)/dx;
     int y = pos(i,1)/dx;
     int z = pos(i,2)/dx;
     Kokkos::atomic_add(&grid(x,y,z), value(i));
   });
+#else
+  auto pos_p = pos.data();
+  auto N = pos.extent(0);
+  auto val_p = value.data();
+  auto grid_p = grid.data();
+  auto grid_y = grid.extent(1);
+#if defined(USE_OMP)
+  auto grid_z = grid.extent(2);
+  #pragma omp parallel for
+  for(int i = 0; i<N; i++) {
+    int x = pos_p[i*3]/dx;
+    int y = pos_p[i*3+1]/dx;
+    int z = pos_p[i*3+2]/dx;
+    // Matching LayoutRight indexing of Kokkos CPU variant
+    #pragma omp atomic update
+    grid_p[(x*grid_y + y)*grid_z + z] += val_p[i];
+  }
+#elif defined(USE_OMPT)
+  auto grid_x = grid.extent(0);
+  #pragma omp target teams distribute parallel for simd \
+                     is_device_ptr(pos_p,grid_p,val_p) \
+                     map(to: N,dx,grid_y,grid_x)
+  for(int i = 0; i<N; i++) {
+    int x = pos_p[i*3]/dx;
+    int y = pos_p[i*3+1]/dx;
+    int z = pos_p[i*3+2]/dx;
+    // Matching LayoutLeft indexing Kokkos GPU variants
+    #pragma omp atomic update
+    grid_p[x + grid_x * (y + z * grid_y)] += val_p[i];
+  }
+#elif defined(USE_CUDA)
+  auto grid_x = grid.extent(0);
+  cuda_kernel<<<(N+128)/128,128>>>(N,[=]__device__(int i) {
+    int x = pos_p[i*3]/dx;
+    int y = pos_p[i*3+1]/dx;
+    int z = pos_p[i*3+2]/dx;
+    // Matching LayoutLeft indexing Kokkos GPU variants
+    atomicAdd(&grid_p[x + grid_x * (y + z * grid_y)], val_p[i]);
+  });
+#endif
+#endif
 }
 
 template<class T>
@@ -91,11 +145,19 @@ int main(int argc, char *argv[]) {
 
   double time_int = run_test<int>(R, dx, pos, grid_size);
   double time_double = run_test<double>(R, dx, pos, grid_size);
+#if defined(USE_KOKKOS)
   double time_complex_float = run_test<Kokkos::complex<float>>(R, dx, pos, grid_size);
+#else
+  double time_complex_float = 1.e99;
+#endif
+#if defined(USE_KOKKOS) && !defined(KOKKOS_ENABLE_OPENMPTARGET) && !defined(KOKKOS_ENABLE_SYCL)
   double time_complex_double = run_test<Kokkos::complex<double>>(R, dx, pos, grid_size);
+#else
+  double time_complex_double = 1.e99;
+#endif
 
   double gup = 1.e-9*num_particles*R;
-  printf("N: %i Grid: %i Time: %e %e %e %e GUPS: %e %e %e %e\n", num_particles, grid_size,
+  printf("N: %i Grid: %i Time: %e %e %e %e GUPS: %2.2lf %2.2lf %2.2lf %2.2lf\n", num_particles, grid_size,
     time_int/R, time_double/R, time_complex_float/R, time_complex_double/R,
     gup/time_int, gup/time_double, gup/time_complex_float, gup/time_complex_double);
   return 0;
