e0ab882f6f6228423b011c01aa03e6e660c9049e
Jakob Bludau <bludauj@ornl.gov>
2025-11-04 10:14:58 -0800

    updated run script

--- benchmarks/vector_ping_pong/run.bash
+++ benchmarks/vector_ping_pong/run.bash
@@ -8,11 +8,11 @@ do
   do
     for stride in 1 2 4 8 16 32
     do
-      for size in 2**10 2**12 2**14 2**16 2**20 2**22
+      for size in 2**8 2**10 2**12 2**14 2**16 2**18 2**20 2**22 2**24 2**26 2**28 2**30
       do
          for variation in "${variations[@]}"
          do
-            ./Kokkos_vector_ping_pong "$variation" 10 "$((size))" "${warmups}" "${pingpongs}" "${stride}"
+            ./vector_ping_pong "$variation" 10 "$((size))" "${warmups}" "${pingpongs}" "${stride}"
          done
        done
     done
