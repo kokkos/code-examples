8f7e2d949d97e6a9ac6a3555cc78a925e7e0640d
Jakob Bludau <bludauj@ornl.gov>
2025-12-02 17:35:56 -0500

    have run scripts for differnt archs

---
+++ benchmarks/vector_ping_pong/run_tests_gh200.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf build_gh200 && cmake -B build_gh200 -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_COMPILER_WARNINGS=ON && cmake --build build_gh200 -j 10
+
+echo "OMP_PROC_BIND=spread OMP_PLACES=threads ./run.bash &> run_gh200.out" | at 22:00
+echo "Queued GH200 job"
