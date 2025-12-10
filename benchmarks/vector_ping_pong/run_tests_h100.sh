b02fc095c86454c2d2abaf548fe7e1c741b044ca
Jakob Bludau <bludauj@ornl.gov>
2025-12-10 10:53:38 -0500

    only attempt to remove directory

--- benchmarks/vector_ping_pong/run_tests_h100.sh
+++ benchmarks/vector_ping_pong/run_tests_h100.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 module load cmake/3.31.1 gcc/12.2.0 cuda/13.0
-rm -rf build_h100 && cmake -B build_h100 -DCMAKE_CXX_COMPILER=g++ -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_COMPILER_WARNINGS=ON && cmake --build build_h100 -j 10
+rm -rf build_h100
+cmake -B build_h100 -DCMAKE_CXX_COMPILER=g++ -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_HOPPER90=ON -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_COMPILER_WARNINGS=ON && cmake --build build_h100 -j 10
 
 echo "OMP_PROC_BIND=spread OMP_PLACES=threads ./run.bash ./build_h100/vector_ping_pong h100 &> run_h100.out" | at 22:00
 echo "Queued H100 job"
--- benchmarks/vector_ping_pong/run_tests_mi300a.sh
+++ benchmarks/vector_ping_pong/run_tests_mi300a.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 module load rocm/6.4.0 cmake/3.31.1
-rm -rf build_mi300a && cmake -B build_mi300a -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_AMD_GFX942_APU=ON -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_COMPILER_WARNINGS=ON -DCMAKE_CXX_COMPILER=hipcc && cmake --build build_mi300a -j 10
+rm -rf build_mi300a
+cmake -B build_mi300a -DKokkos_ENABLE_HIP=ON -DKokkos_ARCH_AMD_GFX942_APU=ON -DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_COMPILER_WARNINGS=ON -DCMAKE_CXX_COMPILER=hipcc && cmake --build build_mi300a -j 10
 
 echo "OMP_PROC_BIND=spread OMP_PLACES=threads HSA_XNACK=1 ./run.bash ./build_mi300a/vector_ping_pong mi300a &> run_mi300a.out" | at 22:00
 echo "Queued MI300A job"
